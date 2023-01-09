8481ba70ba0f9ef6aca16581fa8f9d6900cbf2a8
Kevin Dutzy <kddutz15@gmail.com>
2023-01-09 13:57:15 -0500

    commit tree d58d409033a82f9e1a1cc5dfe254b550dc4860a6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.find(params[:name])
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
